238e47c553f232158c01c6ddd87ee64683bd69a0
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2023-06-18 15:57:23 +0200

    link to status symbols

--- rails_api_app/app/controllers/application_controller.rb
+++ rails_api_app/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+# https://github.com/rails/rails/blob/main/guides/source/layouts_and_rendering.md#the-status-option
 class ApplicationController < ActionController::API
   def append_info_to_payload(payload)
     super
